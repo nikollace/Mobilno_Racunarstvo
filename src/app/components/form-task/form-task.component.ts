81f492883a6c0eb4d1597d0b3f02459392825e79
nikollace <nikoladjordjevicfon@gmail.com>
2021-03-16 21:17:13 +0100

    Dodate edit, delete funkcije

--- src/app/components/form-task/form-task.component.ts
+++ src/app/components/form-task/form-task.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Task } from './task';
 
+
 @Component({
   selector: 'app-form-task',
   templateUrl: './form-task.component.html',
@@ -48,11 +49,11 @@ export class FormTaskComponent {
     this.description = '';
     this.priority = '';
     this.createdAt = '';
-    setTimeout(() => this.setEmptyValues(), 2000);
+    setTimeout(() => this.setEmptyValues(), 3000);
   }
 
   checkAgain(): boolean {
-    if(this.title === 'Title shouldn\'t be empty!' || this.description === 'Description shouldn\'t be empty!' || this.priority === 'Priority shouldn\'t be empty!'){
+    if (this.title === 'Title shouldn\'t be empty!' || this.description === 'Description shouldn\'t be empty!' || this.priority === 'Priority shouldn\'t be empty!') {
       return false;
     }
     return true;
@@ -67,10 +68,45 @@ export class FormTaskComponent {
       if (this.checkAgain()) {
         this.createdAt = new Date().getDate() + "." + (new Date().getMonth() + 1) + "." + new Date().getFullYear() + ".";
         var newTask = new Task(this.title, this.description, this.priority, this.createdAt);
-        this.tasks.push(newTask);
-        this.emptyValues = true;
-        this.clearInputs();
+        var title = this.tasks.find(task => task.title === this.title);
+        if (title) {
+          alert('Task with that title already exist! Please add a new one.');
+        } else {
+          this.tasks.push(newTask);
+          this.emptyValues = true;
+          this.clearInputs();
+        }
       }
     }
+  }
+
+  find(id: Date): any {
+    var task = this.tasks.find(task => task.id === id);
+    return task;
+  }
+
+  saveTask(id: Date): void {
+    var task = this.find(id);
+    task.title = this.title;
+    task.description = this.description;
+    task.priority = this.priority;
+
+    task.editClicked = false;
+
+    this.clearInputs();
+  }
+
+  onEditTask(id: Date): void {
+    var task = this.find(id);
+    this.title = task.title;
+    this.description = task.description;
+    this.priority = task.priority;
+
+    task.editClicked = true;
+  }
+
+  onDeleteTask(id: Date): void {
+    var taskIndex = this.tasks.findIndex(task => task.id === id);
+    this.tasks.splice(taskIndex, 1);
   }
 }
--- src/app/components/form-task/task.ts
+++ src/app/components/form-task/task.ts
@@ -3,10 +3,14 @@ export class Task {
     description: string;
     priority: string;
     createdAt: string;
+    id: Date;
+    editClicked: boolean;
     constructor(title: string, description: string, priority: string, createdAt: string) {
         this.title = title;
         this.description = description;
         this.priority = priority;
         this.createdAt = createdAt;
+        this.id = new Date();
+        this.editClicked = false;
     }
 }
